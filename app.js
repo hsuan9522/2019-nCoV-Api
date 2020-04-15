2263b2331c892dd141b29dd100d99b932a9532be
hsuan9522 <42350575+hsuan9522@users.noreply.github.com>
2020-04-15 16:16:27 +0800

    api

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+app.use(cors(corsOptions));
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
-var express = require("express");
+const express = require("express");
+const request = require("request");
+
 var router = express.Router();
 
 /* GET home page. */
@@ -6,17 +8,33 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-const getStats = async (req, res, next) => {
+const getCumulative = async (req, res, next) => {
   try {
-    const playerStats = {
-      x: 1,
-      y: 2
-    };
-    res.json(playerStats);
+    request.get(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
+      function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          let data = body;
+          data = data.split("\n");
+          let col = data.shift().split(",");
+          let jsonObj = [];
+          data.forEach(el => {
+            let tmpEl = el.split(",");
+            let tmpE = {};
+            tmpEl.forEach((e, index) => {
+              tmpE[col[index]] = e;
+            });
+            jsonObj.push(tmpE);
+          });
+          res.json(jsonObj);
+        }
+      }
+    );
   } catch (e) {
     next(e);
   }
 };
-router.route("/api/v1/stats").get(getStats);
+
+router.route("/api/v1/cumulative").get(getCumulative);
 
 module.exports = router;
